226603c8af4622446c61c89c725fcb63d14ca1a7
Muhammad Rizal <ubeannn@gmail.com>
2025-09-01 14:19:32 +0700

    chore: set minimal PHP version to 8.3

--- src/Example/GlobalInternal.php
+++ src/Example/GlobalInternal.php
@@ -5,6 +5,10 @@
  *
  * A trait declared in the global namespace as `@internal` should be usable only
  * from the global namespace according to the heuristic.
+ *
+ * Can't handle to make only `global` because the current implementation does not have
+ * indicators for internal is present or not (using array length check), so it treats
+ * empty list as public.
  */
 
 namespace {
